8f42e6285df03c4cc14efdfa29d2d89173399168
myrfik <lera.ogay.01@bk.ru>
2024-03-17 20:45:06 +0600

    KS-2-1 average

---
+++ src/main/kotlin/lesson_2_task_1/lesson_2Task_1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_2_task_1
+
+fun main() {
+
+    val mark1: Double = 3.0
+    val mark2: Double = 4.0
+    val mark3: Double = 3.0
+    val mark4: Double = 5.0
+
+    val averageMark: Double = (mark1 + mark2 + mark3 + mark4) / 4
+    println("Средний балл данного класса по английскому языку составляет: $averageMark")
+}
