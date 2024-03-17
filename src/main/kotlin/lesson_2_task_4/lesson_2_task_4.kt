b4d9a57debe4247195af38ede8ed4b0b063853d0
myrfik <lera.ogay.01@bk.ru>
2024-03-17 23:14:35 +0600

    KS-2-4 interest calculation

---
+++ src/main/kotlin/lesson_2_task_4/lesson_2_task_4.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_2_task_4
+
+fun main() {
+    var crystalOre: Int = 7
+    var ironOre: Int = 11
+
+    var bonusCrystalOre: Int = (crystalOre * 20) / 100
+    var bonusIronOre: Int = (ironOre * 20) / 100
+
+    println("Количество бонусных материалов кристаллической руды составляет: $bonusCrystalOre ")
+    println("Количество бонусных материалов железной руды составляет: $bonusIronOre ")
+
+}
+
