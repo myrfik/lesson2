de074176ed7944f334519b112678ed43c585bd00
myrfik <lera.ogay.01@bk.ru>
2024-03-18 17:13:48 +0600

    KS-2-5 refactor

--- src/main/kotlin/lesson_2_task_1/lesson_2Task_1.kt
+++ src/main/kotlin/lesson_2_task_1/lesson_2Task_1.kt
@@ -7,6 +7,8 @@ fun main() {
     val mark3: Double = 3.0
     val mark4: Double = 5.0
 
-    val averageMark: Double = (mark1 + mark2 + mark3 + mark4) / 4
+    val numberOfStudents: Int = 4
+
+    val averageMark: Double = (mark1 + mark2 + mark3 + mark4) / numberOfStudents
     println("Средний балл данного класса по английскому языку составляет: $averageMark")
 }
