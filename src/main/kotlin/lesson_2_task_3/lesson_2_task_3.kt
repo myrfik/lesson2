a9677c5bd33fa2873eb4efbd84dc7d622139c933
myrfik <lera.ogay.01@bk.ru>
2024-03-18 18:28:09 +0600

    KS-2-5 refactor

--- src/main/kotlin/lesson_2_task_3/lesson_2_task_3.kt
+++ src/main/kotlin/lesson_2_task_3/lesson_2_task_3.kt
@@ -1,12 +1,12 @@
 package org.example.lesson_2_task_3
 
 fun main() {
-    var departureHour: Int = 9
-    var departureMinute: Int = 39
-    var travelTime: Int = 457
+    val departureHour: Int = 9
+    val departureMinute: Int = 39
+    val travelTime: Int = 457
 
-    var arrivalHour = (departureHour * MINUTE_IN_HOUR + travelTime) / MINUTE_IN_HOUR
-    var arrivalMinute = (departureHour * MINUTE_IN_HOUR + travelTime) % MINUTE_IN_HOUR
+    val arrivalHour = ((departureHour * MINUTE_IN_HOUR) + departureMinute + travelTime) / MINUTE_IN_HOUR
+    val arrivalMinute = ((departureHour * MINUTE_IN_HOUR) + departureMinute + travelTime) % MINUTE_IN_HOUR
 
     println(String.format("%02d:%02d", arrivalHour, arrivalMinute))
 }
