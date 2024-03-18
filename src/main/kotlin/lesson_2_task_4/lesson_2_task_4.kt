d96e8c1010ab1d8cd572160d2e0ccefeda48ac81
myrfik <lera.ogay.01@bk.ru>
2024-03-18 18:33:20 +0600

    KS-2-4 refactor

--- src/main/kotlin/lesson_2_task_4/lesson_2_task_4.kt
+++ src/main/kotlin/lesson_2_task_4/lesson_2_task_4.kt
@@ -1,11 +1,12 @@
 package org.example.lesson_2_task_4
 
 fun main() {
-    var crystalOre: Int = 7
-    var ironOre: Int = 11
+    val crystalOre: Int = 7
+    val ironOre: Int = 11
+    val bonus: Double = 0.2
 
-    var bonusCrystalOre: Int = (crystalOre * 20) / 100
-    var bonusIronOre: Int = (ironOre * 20) / 100
+    val bonusCrystalOre: Int = (crystalOre * 20) / 100
+    val bonusIronOre: Int = (ironOre * 20) / 100
 
     println("Количество бонусных материалов кристаллической руды составляет: $bonusCrystalOre ")
     println("Количество бонусных материалов железной руды составляет: $bonusIronOre ")
